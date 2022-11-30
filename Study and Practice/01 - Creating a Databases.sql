25f7db2589b5817dd3bf3ca66ee2546f38b58416
Aji Prasetyo <117008370+ajiprasetyo11@users.noreply.github.com>
2022-11-30 21:38:56 +0700

    Add files via upload

---
+++ Study and Practice/01 - Creating a Databases.sql
@@ -0,0 +1,6 @@
+-- Study and Practice: Creating a Database I
+CREATE DATABASE IF NOT EXISTS albums;
+CREATE SCHEMA IF NOT EXISTS albums;
+
+-- Study and Practice: Creating a Database II
+USE albums;
